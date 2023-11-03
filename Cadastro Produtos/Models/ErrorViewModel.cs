3d3ad30cdc59ce60ceeaeabf58ef7e2582bd7adf
hugor <hugorezendemartins1256@gmail.com>
2023-11-03 18:38:59 -0300

    first commit

---
+++ Cadastro Produtos/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Cadastro_Produtos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
